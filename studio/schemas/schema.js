54263b547b8a199ec012c4c00bdc014dc2ed1706
wernecke <wernecke3@gmail.com>
2020-10-14 16:21:34 -0500

    added schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -9,7 +9,7 @@ import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
-
+import listing from './documents/listing'
 // Object types
 import bodyPortableText from './objects/bodyPortableText'
 import bioPortableText from './objects/bioPortableText'
@@ -18,6 +18,29 @@ import mainImage from './objects/mainImage'
 import authorReference from './objects/authorReference'
 
 // Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  // We name our schema
+  name: 'blog',
+  // Then proceed to concatenate our our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    // The following are document types which will appear
+    // in the studio.
+    siteSettings,
+    post,
+    category,
+    author,
+    mainImage,
+    authorReference,
+    bodyPortableText,
+    bioPortableText,
+    excerptPortableText
+
+    // When added to this list, object types can be used as
+    // { type: 'typename' } in other document schemas
+  ])
+})
+
 export default createSchema({
   // We name our schema
   name: 'blog',
