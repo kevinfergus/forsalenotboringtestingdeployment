67dea1d74e65b61f3847f13bb433641057271a3d
wernecke <wernecke3@gmail.com>
2020-10-14 18:31:09 -0500

    added fields to listing schema

--- studio/schemas/documents/listing.js
+++ studio/schemas/documents/listing.js
@@ -1,14 +1,51 @@
-
 export default {
   name: 'listing',
   type: 'document',
   title: 'Listing',
   fields: [
     {
-      name: 'title',
+      name: 'address',
       type: 'string',
-      title: 'Title',
-      description: 'Titles should be catchy, descriptive, and not too long'
+      title: 'Address',
+      description: 'Titles should be catchy, descriptive, and not too long',
     },
-]
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      description: 'Some frontends will require a slug to be set to be able to show the post',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'publishedAt',
+      type: 'datetime',
+      title: 'Published at',
+      description: 'This can be used to schedule post for publishing',
+    },
+    {
+      name: 'mainImage',
+      type: 'mainImage',
+      title: 'Main image',
+    },
+    {
+      name: 'excerpt',
+      type: 'excerptPortableText',
+      title: 'Excerpt',
+      description:
+        'This ends up on summary pages, on Google, when people share your post in social media.',
+    },
+    {
+      name: 'body',
+      type: 'bodyPortableText',
+      title: 'Body'
+    },
+    {
+      name: 'price',
+      type: 'number',
+      title: 'Price'
+    }
+  ],
 }
