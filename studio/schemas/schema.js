9f03a5d8d847ab4780cfd558a2dc307777ed59eb
wernecke <wernecke3@gmail.com>
2020-10-14 19:33:19 -0500

    typo fix

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -9,7 +9,7 @@ import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
-import listing from './documents/listings'
+import listing from './documents/listing'
 // Object types
 import bodyPortableText from './objects/bodyPortableText'
 import bioPortableText from './objects/bioPortableText'
