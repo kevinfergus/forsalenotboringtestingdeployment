81af3bcde067fd9a03e1a8f7e74040abc4270480
wernecke <wernecke3@gmail.com>
2020-10-29 01:55:44 -0500

    format slug page

--- web/src/routes/homes/[slug].svelte
+++ web/src/routes/homes/[slug].svelte
@@ -50,21 +50,38 @@
 </svelte:head>
 <div class="container mx-auto">
   <div
-    class="hero-image w-full bg-cover bg-center"
+    class="hero-image w-full mx-auto bg-cover bg-center"
     style="background-image: url({urlFor(listing.mainImage)})"
     alt={listing.address} />
-<div class="w-full mt-3">
-  <h1 class="text-xl">{listing.address}</h1>
-  <h3 class="text-md text-gray-darker">{listing.city}</h3>
-   <h4>${listing.price}</h4>
-</div>
-<div
-  class="flex w-full py-4 px-2 mb-2 justify-start flex-col sm:flex-row">
-  <div class="w-full sm:w-1/2">
-    <p><strong>{listing.beds}</strong> bed <strong>{listing.baths}</strong> bath</p>
+<div class="w-full md:w-11/12 mt-3">
+  <div class="flex w-full justify-between">
+    <div class="md-w-1/2">
+  <h1 class="text-xl md:text-3xl font-semibold">{listing.address}</h1>
+  <h3 class="text-md md:text-2xl text-gray-darker">{listing.city}</h3>
+    </div>
+    <div>
+      <div class="bg-success rounded-lg px-2 py-1 md:px-3 md:py-2">
+   <span class="tracking-wide md:text-right text-sm md:text-md text-black uppercase">Active</span>
+      </div>
+    </div>
   </div>
-  <div class="w-full sm:w-1/2">
-    <h4>{listing.dateAdded}</h4>
+</div>
+<div class="flex w-full py-4 mb-2 justify-start flex-row">
+  <div class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2">
+    <span class="w-full"><strong>${listing.price}</strong></span>
+    <span class="w-full">price</span>
+  </div>
+  <div class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2">
+    <span class="w-full"><strong>{listing.beds}</strong></span>
+    <span class="w-full">beds</span>
+  </div>
+  <div class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2">
+    <span class="w-full"><strong>{listing.baths}</strong></span>
+    <span class="w-full">baths</span>
+  </div>
+  <div class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2">
+    <span class="w-full"><strong>{listing.sqft}</strong></span>
+    <span class="w-full">sqft.</span>
   </div>
 </div>
 <div class="w-full mx-auto">
