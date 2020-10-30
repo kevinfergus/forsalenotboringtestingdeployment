28d51294713c11ba406ae97642764f3c6b9dcf3a
wernecke <wernecke3@gmail.com>
2020-10-30 01:10:08 -0500

    added vercel and switched scripts to build

--- web/src/routes/api/homes/all.js
+++ web/src/routes/api/homes/all.js
@@ -1,5 +1,6 @@
 import client from '../../../sanityClient'
-
+import urlBuilder from "@sanity/image-url";
+const urlFor = (source) => urlBuilder(client).image(source);
  export async function get (req, res) {
           try {
                         const listings = await client.fetch('*[_type == "listing" && defined(slug.current) && publishedAt < now()]')
