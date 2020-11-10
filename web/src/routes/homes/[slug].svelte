3ce3e2c6daa6d96fee34f32a1d1e3560f50efdd3
wernecke <wernecke3@gmail.com>
2020-11-10 12:42:10 -0600

    using minified css

--- web/src/routes/homes/[slug].svelte
+++ web/src/routes/homes/[slug].svelte
@@ -14,7 +14,6 @@
 
 <script>
   import BlockContent from "@movingbrands/svelte-portable-text";
-  import serializers from "../../components/serializers";
   import urlBuilder from "@sanity/image-url";
   import client from "../../sanityClient";
   const urlFor = (source) => urlBuilder(client).image(source);
