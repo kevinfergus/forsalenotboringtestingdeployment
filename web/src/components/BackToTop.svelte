f20634e0ec3a3c2f45b4d36101fd7fd482ab3c5f
wernecke <wernecke3@gmail.com>
2021-01-26 12:14:03 -0600

    added back to top button

---
+++ web/src/components/BackToTop.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let showOnPx = 550;
+  let hidden = true;
+
+  function goTop() {
+    document.body.scrollIntoView();
+  }
+
+  function scrollContainer() {
+    return document.documentElement || document.body;
+  }
+
+  function handleOnScroll() {
+    if (!scrollContainer()) {
+      return;
+    }
+
+    if (scrollContainer().scrollTop > showOnPx) {
+      hidden = false;
+    } else {
+      hidden = true;
+    }
+  }
+</script>
+
+<style>
+  .back-to-top {
+    opacity: 1;
+    transition: opacity 0.5s, visibility 0.5s;
+    user-select: none;
+    /* left: 50%;
+    transform: translateX(-50%); */
+  }
+
+  .back-to-top.hidden {
+    opacity: 0;
+    visibility: hidden;
+  }
+</style>
+
+<svelte:window on:scroll={handleOnScroll} />
+
+<div class="back-to-top w-40 text-white text-center bg-secondary text-lg font-semibold px-3 py-2 mt-3 rounded-lg mx-auto" on:click={goTop} class:hidden>☝️ Back to Top</div>
--- web/src/routes/homes/[slug].svelte
+++ web/src/routes/homes/[slug].svelte
@@ -1,15 +1,13 @@
 <script context="module">
-    export async function preload({ params }) {
-        try {
-               const res = await this.fetch(`api/homes/${params.slug}`);
-                      const { listing } = await res.json();
-                             return { listing };
-                                  
-        } catch (err) {
-               this.error(500, err);
-                    
-        }
-           };
+  export async function preload({ params }) {
+    try {
+      const res = await this.fetch(`api/homes/${params.slug}`);
+      const { listing } = await res.json();
+      return { listing };
+    } catch (err) {
+      this.error(500, err);
+    }
+  }
 </script>
 
 <script>
@@ -17,16 +15,123 @@
   import serializers from "../../components/serializers";
   import urlBuilder from "@sanity/image-url";
   import client from "../../sanityClient";
+  import BackToTop from "../../components/BackToTop.svelte";
   const urlFor = (source) => urlBuilder(client).image(source);
   export let listing;
   export let images = listing.imageGallery;
   export let price = listing.price;
-        function priceFormat(x) {
-                return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-          };
+  function priceFormat(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
   export let formattedPrice = priceFormat(price);
-
 </script>
+
+<svelte:head>
+  <title>{listing.address} | FSNB</title>
+</svelte:head>
+<div class="container mx-auto mb-2">
+  <div
+    class="hero-image w-full mx-auto bg-cover bg-center rounded"
+    style="background-image: url({urlFor(listing.mainImage)})"
+    alt={listing.address}
+  />
+  <div class="mx-auto w-full mt-3 px-3">
+    <div class="flex w-full justify-between">
+      <div class="md-w-1/2">
+        <h1 class="text-xl md:text-3xl font-semibold">{listing.address}</h1>
+        <h3 class="text-md md:text-2xl text-gray-darker">
+          {listing.city}
+          {#if typeof listing.neighborhood !== "undefined"}| {listing.neighborhood}{/if}
+        </h3>
+      </div>
+      <div>
+        {#if listing.active}
+          <div class="bg-success rounded-lg px-2 py-1 md:px-3 md:py-2">
+            <span
+              class="tracking-wide font-semibold md:text-right text-sm md:text-md text-black uppercase"
+              >Active</span
+            >
+          </div>
+        {:else}
+          <div class="bg-danger rounded-lg px-2 py-1 md:px-3 md:py-2">
+            <span
+              class="tracking-wide font-semibold md:text-right text-sm md:text-md text-black uppercase"
+              >Inactive</span
+            >
+          </div>
+        {/if}
+      </div>
+    </div>
+  </div>
+  <div class="flex w-full py-4 mb-2 justify-start flex-row">
+    <div
+      class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2"
+    >
+      <span class="w-full"><strong>${formattedPrice}</strong></span>
+      <span class="w-full">price</span>
+    </div>
+    <div
+      class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2"
+    >
+      <span class="w-full"><strong>{listing.beds}</strong></span>
+      <span class="w-full">beds</span>
+    </div>
+    <div
+      class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2"
+    >
+      <span class="w-full"><strong>{listing.baths}</strong></span>
+      <span class="w-full">baths</span>
+    </div>
+    <div
+      class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2"
+    >
+      {#if typeof listing.sqft !== "undefined"}
+        <span class="w-full"><strong>{listing.sqft}</strong></span>
+      {:else}
+        <span class="w-full"><strong>-</strong></span>
+      {/if}
+      <span class="w-full">sqft.</span>
+    </div>
+  </div>
+  <div class="w-full mx-auto px-3">
+    <span class="text-xl md:text-2xl font-semibold">Highlight</span>
+    <div class="md:text-xl">
+      <BlockContent blocks={listing.excerpt} />
+    </div>
+  </div>
+  <div class="w-full mt-3 px-3">
+    <span class="text-xl md:text-2xl font-semibold">Notes</span>
+    <div class="md:text-xl">
+      <BlockContent blocks={listing.body} {serializers} />
+    </div>
+  </div>
+  {#if typeof listing.agentName !== "undefined"}
+    <div class="w-full mt-3 mb-2 mx-auto px-3">
+      <span class="text-xl md:text-2xl font-semibold">Listing Agent</span>
+      <ul class="md:text-xl">
+        <li>{listing.agentName}</li>
+        {#if typeof listing.agentNumber !== "undefined"}
+          <li>{listing.agentNumber}</li>
+        {/if}
+        {#if typeof listing.agentEmail !== "undefined"}
+          <li>{listing.agentEmail}</li>
+        {/if}
+      </ul>
+    </div>
+  {/if}
+  <span class="text-xl md:text-2xl font-semibold px-3">Selected Photos</span>
+  {#each images as galleryImg}
+    <div class="my-3 mx-auto">
+      <img
+        class="mx-auto rounded"
+        src="{urlFor(galleryImg)}?auto=format"
+        alt="alt"
+      />
+    </div>
+  {/each}
+
+  <BackToTop />
+</div>
 
 <style>
   .hero-image {
@@ -34,90 +139,7 @@
   }
   @media only screen and (max-width: 600px) {
     .hero-image {
-          height: 270px;
+      height: 270px;
     }
-    
   }
-
 </style>
-
-
-<svelte:head>
-  <title>{listing.address} | FSNB</title>
-</svelte:head>
-<div class="container mx-auto">
-  <div
-    class="hero-image w-full mx-auto bg-cover bg-center rounded"
-    style="background-image: url({urlFor(listing.mainImage)})"
-    alt={listing.address} /><div class="mx-auto w-full mt-3 px-3">
-  <div class="flex w-full justify-between">
-    <div class="md-w-1/2">
-  <h1 class="text-xl md:text-3xl font-semibold">{listing.address}</h1>
-  <h3 class="text-md md:text-2xl text-gray-darker">{listing.city} {#if typeof listing.neighborhood !== 'undefined'}| {listing.neighborhood}{/if}</h3>
-    </div>
-    <div>
-      {#if listing.active}
-      <div class="bg-success rounded-lg px-2 py-1 md:px-3 md:py-2">
-   <span class="tracking-wide font-semibold md:text-right text-sm md:text-md text-black uppercase">Active</span>
-      </div>
-      {:else}
-      <div class="bg-danger rounded-lg px-2 py-1 md:px-3 md:py-2">
-   <span class="tracking-wide font-semibold md:text-right text-sm md:text-md text-black uppercase">Inactive</span>
-      </div>
-      {/if}
-    </div>
-  </div>
-</div>
-<div class="flex w-full py-4 mb-2 justify-start flex-row">
-  <div class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2">
-    <span class="w-full"><strong>${formattedPrice}</strong></span>
-    <span class="w-full">price</span>
-  </div>
-  <div class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2">
-    <span class="w-full"><strong>{listing.beds}</strong></span>
-    <span class="w-full">beds</span>
-  </div>
-  <div class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2">
-    <span class="w-full"><strong>{listing.baths}</strong></span>
-    <span class="w-full">baths</span>
-  </div>
-  <div class="flex flex-col text-center justify-center w-1/4 md:text-2xl sm:w-1/2">
-    {#if typeof listing.sqft !== 'undefined'}
-    <span class="w-full"><strong>{listing.sqft}</strong></span>
-    {:else}
-    <span class="w-full"><strong>-</strong></span>
-    {/if}
-    <span class="w-full">sqft.</span>
-  </div>
-</div>
-<div class="w-full mx-auto px-3">
-  <span class="text-xl md:text-2xl font-semibold">Highlight</span>
-  <div class="md:text-xl">
-  <BlockContent blocks={listing.excerpt} />
-  </div>
-</div>
-<div class="w-full mt-3 px-3">
-  <span class="text-xl md:text-2xl font-semibold">Notes</span>
-  <div class="md:text-xl">
-  <BlockContent blocks={listing.body} {serializers}/>
-  </div>
-</div>
-{#if typeof listing.agentName !== 'undefined'}
-<div class="w-full mt-3 mb-2 mx-auto px-3">
-  <span class="text-xl md:text-2xl font-semibold">Listing Agent</span>
- <ul class="md:text-xl">
-   <li>{listing.agentName}</li>
-    {#if typeof listing.agentNumber !== 'undefined'}
-   <li>{listing.agentNumber}</li>
-    {/if}
-    {#if typeof listing.agentEmail !== 'undefined'}
-   <li>{listing.agentEmail}</li>
-    {/if}
- </ul> 
-</div>
-{/if}
-  <span class="text-xl md:text-2xl font-semibold px-3">Selected Photos</span>
-    {#each images as galleryImg}
-          <div class="my-3 mx-auto"><img class="mx-auto rounded" src="{urlFor(galleryImg)}?auto=format" alt="alt" /></div>
-            {/each}
-</div>
